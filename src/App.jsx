79c5dd3eb1cda380ea1b410ac2f752ed72a7d17c
Himesh Bunkar <himeshdeora@gmail.com>
2026-03-29 02:24:01 +0530

    Create a foreground component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Background from './components/Background'
+import Foreground from './components/Foreground'
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
       <Background/>
-      <div className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/30'></div>
+      <Foreground/>
     </div>
   )
 }
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Foreground() {
+  return (
+    <div className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/30'>
+      <div className='w-60 h-72 bg-sky-800 rounded-[12px]'></div>
+    </div>
+  )
+}
+
+export default Foreground
