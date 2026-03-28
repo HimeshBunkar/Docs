1257f901d08fe6e3c1880c2f130d8a15ffa309e4
Himesh Bunkar <himeshdeora@gmail.com>
2026-03-29 02:15:40 +0530

    Create a backgrond component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Background from './components/Background'
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <div className='absolute w-full py-10 top-[5%] flex justify-center text-xl text-zinc-600 font-semibold'>Documents</div>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900'>DOCS</h1>
+      <Background/>
+      <div className='fixed z-[3] top-0 left-0 w-full h-full bg-sky-800/30'></div>
     </div>
   )
 }
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function Background() {
   return (
     <div>
+      <>
+      <div className='fixed w-full z-[2] h-screen'>
+        <div className='absolute w-full py-10 top-[5%] flex justify-center text-xl text-zinc-600 font-semibold'>Documents</div>
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900'>DOCS</h1>
+      </div>
       
+      </>
     </div>
   )
 }
